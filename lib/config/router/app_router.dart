1caf2bb5df93849c9a379fabbe6819bbe668948f
Andrea Rodrigo <androdmar22@gmail.com>
2025-03-28 15:42:24 +0100

    update shell_routes

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -2,13 +2,16 @@ import 'package:cinemapedia/presentation/screens/screens.dart';
 import 'package:go_router/go_router.dart';
 
 final appRouter = GoRouter(
-  initialLocation: '/',
+  initialLocation: '/home/0',
   routes: [
     
     GoRoute(
-      path: '/',
+      path: '/home/:page',
       name: HomeScreen.name,
-      builder: (context, state) => const HomeScreen(),
+      builder: (context, state) {
+        final pageIndex = int.tryParse(state.pathParameters['page'] ?? '0') ?? 0;
+        return HomeScreen(pageIndex: pageIndex);
+      },
       routes: [
          GoRoute(
           path: 'movie/:id',
@@ -22,7 +25,10 @@ final appRouter = GoRouter(
       ]
     ),
 
-
+    GoRoute(
+      path: '/',
+      redirect: ( _ , __) => '/home/0',
+    )
 
   ]
 );
