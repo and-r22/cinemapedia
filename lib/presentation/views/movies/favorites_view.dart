a3647f2b2ca6356d3b6ef9ba8b62b026cb1aea8a
Andrea Rodrigo <androdmar22@gmail.com>
2025-03-31 15:11:52 +0200

    add no favorites view

--- lib/presentation/views/movies/favorites_view.dart
+++ lib/presentation/views/movies/favorites_view.dart
@@ -2,6 +2,7 @@ import 'package:cinemapedia/presentation/providers/providers.dart';
 import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 class FavoritesView extends ConsumerStatefulWidget {
   const FavoritesView({super.key});
@@ -45,6 +46,32 @@ class FavoritesViewState extends ConsumerState<FavoritesView> {
   Widget build(BuildContext context) {
 
     final favoriteMovies = ref.watch(favoriteMoviesProvider).values.toList();
+
+    if (favoriteMovies.isEmpty) {
+      final colors = Theme.of(context).colorScheme;
+      return Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+
+            Icon(Icons.favorite_outline_sharp, size: 60, color: colors.primary,),
+
+            Text('Ohhh no!!!', style: TextStyle(fontSize: 30, color: colors.primary)),
+
+            const Text('No tienes películas en favoritos', style: TextStyle(fontSize: 20, color: Colors.black45)),
+
+            const SizedBox(height: 20,),
+
+            FilledButton.tonal(
+              onPressed: () => context.go('/home/0'), 
+              child: const Text('Empieza a buscar'),
+            )
+
+          ],
+        ),
+      );
+    }
 
     return Scaffold(
       body: MoviesMasonry(
